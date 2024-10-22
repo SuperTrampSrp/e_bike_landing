d7988b764f356b3183484eddf3bb93d1c1ce0c96
SuperTrampSrp <65632897+SuperTrampSrp@users.noreply.github.com>
2024-10-22 11:59:54 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,101 @@
 import Image from "next/image";
+import NavBar from "./components/NavBar";
+import { GoSearch } from "react-icons/go";
+import { GrFormPrevious } from "react-icons/gr";
+import { GrFormNext } from "react-icons/gr";
+import { GoDotFill } from "react-icons/go";
+import { IoIosAdd } from "react-icons/io";
+
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col bg bg-gradient-to-br from-sky-300 to bg-cyan-400 h-screen py-16 px-36 font-sans">
+      <div className="flex flex-col bg-gradient-to-r from-cyan-300 to-blue-400 h-full border-solid rounded-3xl border-2">
+        <nav className="flex justify-between w-full p-10 items-center text-black font-bold text-sm">
+          <ul className="flex  justify-center w-full gap-10 items-center">
+            <li>HOME</li>
+            <li>PRODUCTS</li>
+            <li>GALLERY</li>
+            <li>CONTACT</li>
+            <li>
+              <button className=" px-6 text-white font-semibold py-2 rounded-lg bg-teal-600 border-teal-400 border-2 border-solid">LOGIN</button>
+            </li>
+          </ul>
+          <div className="z-10">
+            <GoSearch size={30} color="white" />
+          </div>
+        </nav>
+        <main className="flex text-black h-full">
+          <div className="flex flex-col w-5/12 justify-center h-full items-center">
+            <div className=" flex justify-center flex-col w-10/12">
+              <h1 className="text-2xl uppercase tracking-[15px]">let's ride the</h1>
+              <h1 className="text-8xl uppercase font-bold -ml-1">future</h1>
+              <div className="flex flex-col text-left w-10/12 mt-10">
+                <p>simple and sleek design</p>
+                <p>with users mind</p>
+              </div>
+            </div>
+            <div className="flex w-10/12 mt-10">
+              <button className=" px-8 py-2 rounded-lg bg-teal-600 border-teal-400 border-2 border-solid text-sm text-white text-left">LOGIN</button>
+            </div>
+          </div>
+          <div className="flex flex-col w-6/12 h-full justify-between items-center">
+            <h1 className="text-9xl text-white text-end mt-10 ml-28">EV-B</h1>
+            <div className="absolute mr-44 mt-16">
+              <Image
+                height={500}
+                width={500}
+                src='/bike.png'
+                className=""
+              />
+            </div>
+            <div className="flex justify-center bg-blue-200 px-10 rounded-xl bg-opacity-25 border-solid border-2 border-cyan-300 py-5 gap-8">
+              <div className="flex flex-col text-start">
+                <span className="text-md font-semibold">50 Km/hr</span>
+                <span>speed</span>
+              </div>
+              <div className="flex flex-col text-start">
+                <span className="text-md font-semibold">80 Km</span>
+                <span>battery range</span>
+              </div>
+              <div className="flex flex-col text-start">
+                <span className="text-md font-semibold">3.5 hr</span>
+                <span>charging time</span>
+              </div>
+            </div>
+          </div>
+          <div className=" flex w-1/12 flex-col -z-0">
+            <div className="bg-gradient-to-br from-teal-200 to-teal-500 h-full -mt-[119px] rounded-tr-[22px] w-full pt-36">
+              <h1 className="text-white rotate-90 tracking-[10px] w-full uppercase whitespace-nowrap">the concept</h1>
+            </div>
+            <div className="bg-gradient-to-br from-cyan-200 to-cyan-500 h-full -mb-[80px] rounded-br-[22px] w-full pt-20">
+              <h1 className="text-white rotate-90 tracking-[10px] w-full uppercase whitespace-nowrap">electric - b</h1>
+            </div>
+            <div className="p-2 text-white rounded-full bottom-20 right-56 absolute bg-teal-500">
+              <IoIosAdd size={40} />
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </main>
+        <footer className="h-20 flex justify-between px-16 py-5 pr-36">
+          <div className="flex items-center font-semibold text-slate-700">
+            <GrFormPrevious size={50} />
+            <span>Prev</span>
+          </div>
+          <div className="flex items-center text-slate-300">
+            <span><GoDotFill /></span>
+            <span><GoDotFill /></span>
+            <span><GoDotFill /></span>
+            <span><GoDotFill /></span>
+          </div>
+          <div className="flex items-center font-semibold text-slate-700">
+            <span> Next</span>
+            <GrFormNext size={50} />
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
